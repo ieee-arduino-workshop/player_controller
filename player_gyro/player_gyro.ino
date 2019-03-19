4d2112981ad40d5619833c1f6fab2b0db324b288
Long Tran <qlong1505@gmail.com>
2019-03-20 01:17:35 +1100

    add packet header file. Write movement value to packet.

--- player_gyro/player_gyro.ino
+++ player_gyro/player_gyro.ino
@@ -1,28 +1,33 @@
 /*
 	18/02/2019 by Long Tran:  create a test project to read x_gyro and y_gyro sensor. 
 */
-#include"MPU6050.h" //call gyro sensor library
+#include "MPU6050.h"
+#include "packet.h"    //call gyro sensor library
 #define THRESHOLD 5000 //sensitivity value (-32767 to 32768) for direction decision
 
 // Global variables
 //  Constants variables
-const int button_pin = 2;
-const int led_pin = 13;
+#define button_pin 2
+#define led_pin 13
 //  Dynamic variables
 int button_state = 0;
 
-void setup()
-{      
-	// two variable below store the return data from MPU6050_read() function
-  int error;	
-  uint8_t c; 
+packet Packet;
+int error;
+accel_t_gyro_union accel_t_gyro;
+char a[16];
 
-	// Initialize serial port for monitoring
-  Serial.begin(9600);
-  
+void setup()
+{
+  // two variable below store the return data from MPU6050_read() function
+  int error;
+  uint8_t c;
+  Packet.packet_data = 0xFFFF;
+  // Initialize serial port for monitoring
+  Serial.begin(115200);
+
   // Initialize the 'Wire' class for the I2C-bus.
   Wire.begin();
-
 
   // MPU6050 default at power-up:
   //    Gyro at 250 degrees second
@@ -30,24 +35,24 @@ void setup()
   //    Clock source at internal 8MHz
   //    The device is in sleep mode.
   //
-  error = MPU6050_read (MPU6050_WHO_AM_I, &c, 1);
+  error = MPU6050_read(MPU6050_WHO_AM_I, &c, 1);
   Serial.print(F("WHO_AM_I : "));
-  Serial.print(c,HEX);
+  Serial.print(c, HEX);
   Serial.print(F(", error = "));
-  Serial.println(error,DEC);
+  Serial.println(error, DEC);
 
   // According to the datasheet, the 'sleep' bit
   // should read a '1'.
   // That bit has to be cleared, since the sensor
-  // is in sleep mode at power-up. 
-	error = MPU6050_read (MPU6050_PWR_MGMT_1, &c, 1);
+  // is in sleep mode at power-up.
+  error = MPU6050_read(MPU6050_PWR_MGMT_1, &c, 1);
   Serial.print(F("PWR_MGMT_1 : "));
-  Serial.print(c,HEX);
+  Serial.print(c, HEX);
   Serial.print(F(", error = "));
-  Serial.println(error,DEC);
+  Serial.println(error, DEC);
 
   // Clear the 'sleep' bit to start the sensor.
-  MPU6050_write_reg (MPU6050_PWR_MGMT_1, 0);
+  MPU6050_write_reg(MPU6050_PWR_MGMT_1, 0);
 
   // Setting the pins
   //  Initialising the LED as an output for testing purposes
@@ -56,59 +61,83 @@ void setup()
   pinMode(button_pin, INPUT);
 }
 
+
 void loop()
 {
-  int error;
-  accel_t_gyro_union accel_t_gyro;
-
   // Read the raw values.
-  // Read 4 bytes at once, 
+  // Read 4 bytes at once,
   // containing x_gyro (2 bytes) and y_gyro (2 bytes).
   // With the default settings of the MPU-6050,
   // there is no filter enabled, and the values
   // are not very stable. But they are fine for left-right,up-down determination.
-  error = MPU6050_read (MPU6050_ACCEL_XOUT_H, (uint8_t *) &accel_t_gyro, sizeof(accel_t_gyro));
-	
-	//uncomment code below for debugging purposes.
-	/*  Serial.print(F("Read accel, temp and gyro, error = "));
+  error = MPU6050_read(MPU6050_ACCEL_XOUT_H, (uint8_t *)&accel_t_gyro, sizeof(accel_t_gyro));
+
+  //uncomment code below for debugging purposes.
+  /*  Serial.print(F("Read accel, temp and gyro, error = "));
 	  Serial.println(error,DEC);
 	*/
 
   // Swap all high and low bytes.
-  // After this, the registers values are swapped, 
+  // After this, the registers values are swapped,
   // Why has to swap?
-	// The AVR chip (on the Arduino board) has the Low Byte 
-	// at the lower address.
-	// But the MPU-6050 has a different order: High Byte at
-	// lower address, so that has to be corrected.
-	// write function SWAP
-	uint8_t swap;
-  #define SWAP(x,y) swap = x; x = y; y = swap
+  // The AVR chip (on the Arduino board) has the Low Byte
+  // at the lower address.
+  // But the MPU-6050 has a different order: High Byte at
+  // lower address, so that has to be corrected.
+  // write function SWAP
+  uint8_t swap;
+#define SWAP(x, y) \
+  swap = x;        \
+  x = y;           \
+  y = swap
 
-	//swap low and high byte of x_gyro and y_gyro
-  SWAP (accel_t_gyro.reg.x_gyro_h, accel_t_gyro.reg.x_gyro_l);
-  SWAP (accel_t_gyro.reg.y_gyro_h, accel_t_gyro.reg.y_gyro_l);
+  //swap low and high byte of x_gyro and y_gyro
+  SWAP(accel_t_gyro.reg.x_gyro_h, accel_t_gyro.reg.x_gyro_l);
+  SWAP(accel_t_gyro.reg.y_gyro_h, accel_t_gyro.reg.y_gyro_l);
 
-	// Determine direction
-	// Compare x_gyro and y_gyro value to a THRESHOLD number - it can be calibrated for the sensitivity 
-		// process x_gyro
-	if(accel_t_gyro.value.x_gyro<-THRESHOLD)
-		Serial.print(F("RIGHT \t"));  
+  // Determine direction
+  // Compare x_gyro and y_gyro value to a THRESHOLD number - it can be calibrated for the sensitivity
+  // process x_gyro
+  if (accel_t_gyro.value.x_gyro < -THRESHOLD)
+  {
+    Serial.print(F("RIGHT \t"));
+    Packet.right = 1;
+  }
+  else if (accel_t_gyro.value.x_gyro > THRESHOLD)
+  {
+    Serial.print(F("LEFT  \t"));
+    Packet.left = 1;
+  }
   else
-    if(accel_t_gyro.value.x_gyro>THRESHOLD) 
-			Serial.print(F("LEFT  \t"));  
-    else
-			Serial.print(F("      \t"));  
-							
-		//process y_gyro
-  if(accel_t_gyro.value.y_gyro>THRESHOLD)
-		Serial.print(F("UP   \n"));  
+  {
+    Serial.print(F("      \t"));
+    //Packet.packet_data = Packet.packet_data & 0xFFE7 ;// 11111111 11100111;
+    Packet.left = 0;
+    Packet.right = 0;
+  }
+  //process y_gyro
+  if (accel_t_gyro.value.y_gyro > THRESHOLD)
+  {
+    Serial.print(F("UP   \n"));
+    Packet.up = 1;
+  }
+  else if (accel_t_gyro.value.y_gyro < -THRESHOLD)
+  {
+    Serial.print(F("DOWN  \n"));
+    Packet.down = 1;
+  }
   else
-    if(accel_t_gyro.value.y_gyro<-THRESHOLD)
-			Serial.print(F("DOWN  \n"));  
-    else
-			Serial.print(F("      \n"));          
-  
+  {
+    Serial.print(F("      \n"));
+    Packet.down = 0;
+    Packet.up = 0;
+  }
+  Serial.println("---RLDUK|--ID--|");
+
+  //a = itoa(Packet.packet_data, a, 2);
+  Serial.println(Packet.packet_data, BIN);
+  //Serial.println(a);
+
   // Read the state of the push button
   button_state = digitalRead(button_pin);
 
@@ -117,12 +146,12 @@ void loop()
   {
     // Turn ON the LED
     digitalWrite(led_pin, HIGH);
-  } 
+  }
   else
   {
     // Turn OFF the LED
     digitalWrite(led_pin, LOW);
   }
-  
-  delay(100);
+
+  delay(200);
 }
